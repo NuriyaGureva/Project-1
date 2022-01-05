0c75f58be3703273c4516746ee882fde2df7f50c
gureva.anj@gmail.com <gureva.anj@gmail.com>
2022-01-05 14:20:13 +0300

    Binary WORK_HOME

---
+++ Binary/Binary.cpp
@@ -0,0 +1,206 @@
+﻿// Binary.cpp 
+
+#include <iostream>
+using namespace  std;
+
+
+class Binary
+{
+	unsigned int capacity;//Разрядность числа
+	bool* number;//Двоичное число
+
+public:
+	unsigned int get_capacity()const
+	{
+		return capacity;
+	}
+	const bool* get_number()const
+	{
+		return number;
+	}
+	bool* get_number()
+	{
+		return number;
+	}
+	void set_capacity(int capacity)
+	{
+		if (capacity > 32)capacity = 32;
+		bool* new_number = new bool[capacity] {};
+		for (int i = 0; i < (this->capacity < capacity ? this->capacity : capacity); i++)
+			new_number[i] = this->number[i];
+		this->capacity = capacity;
+		delete[] this->number;
+		this->number = new bool[capacity] {};
+
+	}
+
+	Binary()
+	{
+		capacity = 8;
+		number = new bool[capacity] {};
+		cout << "DefaultConstructor:" << this << endl;
+
+	}Binary(int decimal)
+	{
+		int buffer = decimal;
+		//
+		capacity = 0;
+		for (; buffer; capacity++)buffer /= 2;
+		if (capacity <= 8)capacity = 8;
+		else if (capacity <=16)capacity = 16;
+		else if (capacity <= 24)capacity = 24;
+		else if (capacity <= 16)capacity = 16;
+
+		number = new bool[capacity] {};
+		for (int i = 0; decimal; i++)
+		{
+			number[i] = decimal % 2;
+			decimal /= 2;
+		}
+
+	}
+	Binary(const Binary& other)
+	{
+		this->capacity = other.capacity;
+		this->number = new bool[capacity] {};
+		for (int i = 0; i < capacity; i++)
+		{
+			this->number[i] = other.number[i];
+		}
+		cout << "CopyConstructor:" << this << endl;
+
+	}
+	Binary(Binary&& other)
+	{
+		this->capacity = other.capacity;
+		this->number = other.number;
+		other.number = nullptr;
+		other.capacity = 0;
+		cout << " MoveConstructop:\t" << this << endl;
+	}
+
+	~Binary()
+	{
+		delete[] number;
+		number = nullptr;
+		capacity = 0;
+		cout << "Destructor:\t" << this << endl;
+	}
+	//       Operators;
+	Binary& operator=(const Binary& other)
+	{
+
+		if (this == &other)return *this;
+		delete[]this->number;
+		this->capacity = other.capacity;
+		this->number = new bool[capacity] {};
+		for (int i = 0; i < capacity; i++)this->number[i] = other.number[i];
+		cout << "CopyAssigment:\t" << this << endl;
+		return *this;
+	}
+	Binary& operator=(Binary&& other)
+	{
+		if (this == &other)return *this;
+		delete[]number;
+		this->capacity = other.capacity;
+		this->number = other.number;
+		other.number = nullptr;
+		other.capacity = 0;
+		cout << " MoveAssigment:\t" << this << endl;
+		return *this;
+	}
+
+	//				Methods
+	Binary operator~()const
+	{
+		Binary inversion = *this;
+		for (int i = 0; i < capacity; i++)
+		{
+			//if (inversion.number[i] == 1)inversion.number[i];
+			//else inversion.number[i] = 1;
+			inversion.number[i] = inversion.number[i] ? 0 : 1;
+		}
+		return inversion;
+	}
+	std::ostream& print(std::ostream& os = std::cout)const
+	{
+		if (capacity == 8)os.width(11 * 3);
+		if (capacity == 16)os.width(11 * 2);
+		if (capacity == 24)os.width(11);
+		for (int i = capacity - 1; i >= 0; i--)
+		{
+			os << number[i];
+			if (i % 8 == 0)cout << " ";
+			if (i % 4 == 0)cout << " ";
+		}
+		return os;
+	}
+
+
+};
+
+Binary operator|(const Binary& left, const Binary& right)
+{
+	Binary result;
+	int max_capacity = left.get_capacity() < right.get_capacity() ?
+		right.get_capacity() : left.get_capacity();
+	int min_capacity = left.get_capacity() > right.get_capacity() ?
+		right.get_capacity() : left.get_capacity();
+	result.set_capacity(max_capacity);
+	for (int i = 0; i < min_capacity; i++)
+		result.get_number()[i] = left.get_number()[i] || right.get_number()[i] ? 1 : 0;
+	for (int i = min_capacity; i < max_capacity; i++)
+		result.get_number()[i] = left.get_capacity() > right.get_capacity() ?
+		left.get_number()[i] : right.get_number()[i];
+	return result;
+
+}
+Binary operator&(const Binary& left, const Binary& right)
+{
+	Binary result;
+	int capacity = left.get_capacity() < right.get_capacity()?
+		right.get_capacity() : left.get_capacity();
+	for (int i = 0; i < capacity; i++)
+		result.get_number()[i] = left.get_number()[i] && right.get_number()[i] ? 1 : 0;
+	return result;
+}
+Binary operator^(const Binary& left, const Binary& right)
+{
+	Binary result;
+	int capacity = left.get_capacity() < right.get_capacity()?
+		right.get_capacity() : left.get_capacity();
+	for (int i = 0; i < capacity; i++)
+		result.get_number()[i] = left.get_number()[i] ^ right.get_number()[i] ? 1 :0 ;
+		return result;
+		
+}
+std::ostream& operator<<(std::ostream& os, const Binary& obj)
+{
+	return obj.print(os);
+}
+//#define CONSTRUCTORS_CHECK
+int main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef CONSTRUCTORS_CHECK
+	Binary num1;
+	num1.print();
+	cout << endl;
+
+	Binary num2 = 380;//Двоичное число иници
+	cout << num2 << endl;
+
+	Binary num3;
+	num3 = num2;
+	cout << ~num3 << endl;
+#endif // CONSTRUCTORS_CHECK
+
+	Binary bin1 =202;
+	Binary bin2 =27;
+
+	cout << (bin1 | bin2) << endl;
+	cout << (bin1 & bin2) << endl;
+	cout << (bin1 ^ bin2) << endl;
+}
+
--- Class String/Class String.cpp
+++ Class String/Class String.cpp
@@ -6,86 +6,223 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimiter "\n-------------------------------------------\n"
+
+class String;
+String operator+(const String& left, const String& right);
+
 
 class String
-{	
-	std::string str;
-	std::string str1;
-	std::string str2;
-	std::string str3;
-	std::string str4;
-public: 
-	std::string get_str()const
+{
+	int size; //Размер строки В байтах
+	char* str;// Память строки динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
 	{
 		return str;
 	}
-	std::string get_str1()const
+	char* get_str()
 	{
-		return str1;
+		return str;
 	}
-	std::string get_str2()const
+	explicit String(int size = 80):size(size),str(new char[size]{})
 	{
-		return str2;
+		//this->size = size;
+		//this->str = new char[size] {};//Память выделяемую для строк
+		//обязательно нужно занулить
+		cout << "SizeConstructor:\t" << this << endl;
 	}
-	void set_str(std:: string str)
+	/*String(const char str[])
 	{
-		this->str = str;
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; str[i]; i++)
+			this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
+	}*/
+	/*String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+			this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+	}*/
+
+	String(const char str[]) :String(strlen(str) + 1)
+	{
+		for (int i = 0; str[i]; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
 	}
-	void set_str1(std::string str1)
+
+	String(String&& other)noexcept//никогда не бросает исключение
 	{
-		this->str1 = str1;
+		//MoveConctructop Должен выполнять поверхностное копирование
+		this->size = other.size;
+		this->str = other.str;// Просто копируем адрес память,
+		//принадлежащей другому объекту.
+		// Зануляем свой объек для того,
+		//чтобы его память не мог удалить сам деструктор.
+		other.size = 0;
+		other.str = nullptr;
+	
+		cout << "MoveConstructor" << this << endl;
 	}
-	void set_str2(std::string str2)
+	String(const String& other) :String(other.str)
 	{
-		this->str2 = str2;
-	}
-	String() 
-	{
-		this->str;
-		this->str1;
-		this->str2;
-		
-		cout << "DefaultConstruct:\t" << this << endl;
-	}
-	String(const char* str)
-	{
-		this->str = str;
-		this->str1 = str1;
-		this->str2 = str2;
-		cout << "lagrConstructor:\t" << this << endl;
+		cout << "CopyConstructor:\t" << this << endl;
 	}
 	~String()
 	{
-		cout << "Destructor:\t" << endl;
+		delete[]str;
+		cout << "Destructor:\t" << this << endl;
 	}
-		
-};  
+	//            Operators:
 
-	 std::ostream& operator<<(std::ostream& os, const String& str)
-	 {		
-		 return os;
-	 }    
+	String& operator=(const String& other)
+	{
+		if (this == &other)return *this;//Проверяем не является ли this
+		//и other одним и тем же объектом
+		delete[] this->str;
+		//Deep copy(Побитовое копирование)
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+		this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+	const char& operator[](int i)const
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+	}
 
-    std::string str;	
-	std::string str1 = "Hello,";	
-	std::string str2 = "World!";
-	std::string str3 = str1 + str2 ;
-	std::string str4; 
+	String& operator=(String&& other)
+	{
+		if (this == &other)return *this;
+		delete this->str;
+		this->size = other.size;
+		this->str = other.str;
 
+		other.size = 0;
+		other.str = nullptr;
+		cout << " MoveAssignment;\t" << this << endl;
+		return *this;
+	}
+	// Metods
+	void print()const
+	{
+		cout << " Size:\t"<< size << endl;
+		cout << " Str:\t" << str << endl;
+	}
+};
 
-int main()
-{   
+String operator + (const String & left, const String & right)
+{
+	String result(left.get_size() + right.get_size() - 1);
+	for (int i=0; i < left.get_size(); i++)
+		result[i] = left[i];
+		//result.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		result[i + left.get_size() - 1] = right[i];
+		//result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return result;
+}
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+std::istream& operator>>(std::istream& is, String& obj)
+{
+	//return is >> obj.get_str();
+	const int SIZE = 1024 * 1000;
+	char buffer[SIZE] = {};
+	is >> buffer;
+	obj = buffer;
+	return is;
+}
+
+std::istream& getline(std::istream& is, String& obj)
+{
+	const int SIZE = 1024 * 1000;
+	char buffer[SIZE] = {};
+	is.getline(buffer, SIZE);
+	obj = buffer;
+	return is;//winver
+}
+
+//std::ostream& operator<<(std::ostream& os, const String& obj)
+//{
+//	return os << obj.get_str();
+//}
+ 
+// #define COSRUCTORS_CHECK
+#define OPERATORS_CHECK
+//#define INPUT_CHECK
+
+void main()
+{
 	setlocale(LC_ALL, "Russian");
 
-	String str("80");
-    String str1 = "Hello";
+#ifdef COSRUCTORS_CHECK
+	String str1;// Конструктор по умолчанию
+	str1.print();
+	cout << str1 << endl;
+
+	String str2(25);
+	str2.print();
+
+	String str3 = "Hello";
+	str3 = str3;
+	cout << str3 << endl;
+
+	String str4 = str3;
+	cout << str4 << endl;
+
+	String str5;
+	str5 = str3;
+	cout << str5 << endl;
+#endif // COSRUCTORS_CHECK
+
+#ifdef OPERATORS_CHECK
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	String str1 = "Hello";
 	String str2 = "World";
-	String str4("25");
-	//String str3 = str1 + str2;
-	std::cout << "str: " << endl;
-	std::cout << "str1: " << "Hello," << endl;
-	std::cout << "str2: " << "Word!" << endl;
-	std::cout << str3 << endl;
-	std::cout << "str4: " << endl;
+	cout << delimiter << endl;
+	String str3 = str1 + str2;
+	cout << delimiter << endl;	
+	cout << str3 << endl;
+	cout << delimiter << endl;
+	str1 += str2;
+	cout << delimiter << endl;
+	cout << str1 << endl;
+	cout << delimiter << endl;
+#endif // OPERATORS_CHECK
+
+#ifdef INPUT_CHECK	
+	String str;
+	cout << "Введите строку: ";
+	//cin >> str;
+	getline(cin, str);
+	cout << str << endl;
+	str.print();
+#endif // INPUT_CHECK	
+
+
 }
 
